89a48588f03d314363d05fc8f1dddb294093ab53
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-02 22:44:14 -0400

    added attributes to User

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,53 @@
 package entity;
 
-public class User {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class User implements Serializable {
+    private ArrayList<String> connectionRequests;
+    private ArrayList<String> pendingConnections;
+    private ArrayList<String> connections;
+    private final String id;
+    private String name;
+    public User(String id, ArrayList<String> connectionRequests, ArrayList<String> pendingConnections,
+                ArrayList<String> connections, String name) {
+        this.connectionRequests = connectionRequests;
+        this.pendingConnections = pendingConnections;
+        this.connections = connections;
+        this.id = id;
+        this.name = name;
+    }
+
+    public ArrayList<String> getConnectionRequests() {
+        return connectionRequests;
+    }
+
+    public void setConnectionRequests(ArrayList<String> connectionRequests) {
+        this.connectionRequests = connectionRequests;
+    }
+
+    public ArrayList<String> getPendingConnections() {
+        return pendingConnections;
+    }
+
+    public void setPendingConnections(ArrayList<String> pendingConnections) {
+        this.pendingConnections = pendingConnections;
+    }
+
+    public ArrayList<String> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(ArrayList<String> connections) {
+        this.connections = connections;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
