8bac2417027ed348bab14a179721ad27e0049513
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-12 14:49:29 -0500

    add bad request representation

---
+++ src/main/java/representation/BadRequestResponseRepresentation.java
@@ -0,0 +1,18 @@
+package representation;
+
+public class BadRequestResponseRepresentation {
+
+    private String content;
+
+    public BadRequestResponseRepresentation() {
+    }
+
+    public BadRequestResponseRepresentation(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
