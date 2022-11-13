0e7ec737f9b1a5c7c6975e7bfa199b380cb8c983
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-12 23:47:20 -0500

    set up db access

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,49 +1,43 @@
 package main;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import representation.ExampleReceive;
 import representation.ExampleSend;
-import main.Controller;
 
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        Controller controller = new Controller();
-        controller.myAction();
-
-//        Object[] parameters = new Object[] {new Integer(2)};
-//        Object o = jt.queryForObject("select name from elephant where id = ?",
-//                parameters, String.class);
-//        System.out.println((String)o);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+    @Override
+    public void run(String... args) throws Exception {
 
-    @MessageMapping("/example")
-    // broadcasts to all subscribers of /topic/example
-    @SendTo("/topic/example")
-    public ExampleSend example(ExampleReceive received) throws Exception {
-        Thread.sleep(1000);
-        return new ExampleSend();
+//        jdbcTemplate.execute(
+//                "create table students (name varchar, email varchar)"
+//        );
+
+        String sql = "INSERT INTO students (name, email) VALUES ("
+                + "'Leo', 'leo@utoronto.ca')";
+
+        int rows = jdbcTemplate.update(sql);
+        if (rows > 0) {
+            System.out.println("A new row has been inserted.");
+        }
     }
 }
