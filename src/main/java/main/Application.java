2679ff5f2f1383ef4f344e2750b522c4c4c5f69e
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-13 15:04:35 -0500

    Connected Spring to ElephantSql

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,43 +1,26 @@
 package main;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import representation.ExampleReceive;
-import representation.ExampleSend;
+import user.connect.ConnectionDataAccess;
 
 
 @SpringBootApplication
 @RestController
-public class Application implements CommandLineRunner {
+@ComponentScan("user.connect")
+public class Application{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private ConnectionDataAccess dataAccess;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-//        jdbcTemplate.execute(
-//                "create table students (name varchar, email varchar)"
-//        );
-
-        String sql = "INSERT INTO students (name, email) VALUES ("
-                + "'Leo', 'leo@utoronto.ca')";
-
-        int rows = jdbcTemplate.update(sql);
-        if (rows > 0) {
-            System.out.println("A new row has been inserted.");
-        }
     }
 }
