2dd5c1e1291c81e1d441bea0ca0a93b68f707b79
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-13 20:11:11 -0500

    Implement controller, DAO and handler

--- src/main/java/user/acceptconnect/AcceptConnectionController.java
+++ src/main/java/user/acceptconnect/AcceptConnectionController.java
@@ -1,4 +1,12 @@
 package user.acceptconnect;
 
 public class AcceptConnectionController {
+    final IAcceptConnectionInput interactor;
+    public AcceptConnectionController(IAcceptConnectionInput interactor){
+        this.interactor = interactor;
+    }
+    public AcceptConnectionResponseModel acceptConnection(String userName, String targetName){
+        AcceptConnectionRequestModel requestModel = new AcceptConnectionRequestModel(userName, targetName);
+        return interactor.acceptConnection(requestModel);
+    }
 }
