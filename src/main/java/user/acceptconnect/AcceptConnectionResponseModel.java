415ed34fa15d13bfbcf3a0498360caf219213248
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-13 20:12:51 -0500

    Add request/response model

--- src/main/java/user/acceptconnect/AcceptConnectionResponseModel.java
+++ src/main/java/user/acceptconnect/AcceptConnectionResponseModel.java
@@ -1,4 +1,26 @@
 package user.acceptconnect;
 
 public class AcceptConnectionResponseModel {
+    private String status;
+    private String message;
+    public AcceptConnectionResponseModel(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
