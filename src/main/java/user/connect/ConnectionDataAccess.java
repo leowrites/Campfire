15f95ce825ede75cad26aecec5e92fbc13097398
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-13 22:20:37 -0500

    Add user row mapper and implement query with sql strings

--- src/main/java/user/connect/ConnectionDataAccess.java
+++ src/main/java/user/connect/ConnectionDataAccess.java
@@ -13,45 +13,20 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ConnectionDataAccess implements IConnectionDataAccess {
-    /**
-     * @param userId id of the current user
-     * @return A serialized user object
-     */
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    final String INSERT_QUERY = "INSERT INTO users (" +
-            "connectionRequests, " +
-            "pendingConnections, " +
-            "connections, name) " +
-            "values (?, ?, ?, ?)";
-    final String UPDATE_QUERY = "update user set ? = ? where id = ?";
-    @Override
-    public User getUser(String userId) {
-        /**
-         * TODO
-         *  need to implement this with mySQL query
-         *  this is a temporary placeholder
-         */
-        User user = null;
-        try {
-            // Reading the object from a file
-            FileInputStream file = new FileInputStream
-                    ("fakeUser.txt");
-            ObjectInputStream in = new ObjectInputStream
-                    (file);
-            // Method for deserialization of object
-            user = (User)in.readObject();
-            in.close();
-            file.close();
-        }
+    final String INSERT_QUERY = "INSERT INTO users (username, data) values (?, ?)";
+    final String UPDATE_QUERY = "update user set ? = ? where username = ?";
+    final String DATA_QUERY = "select username, data from users where username = ? ";
 
-        catch (IOException ex) {
-            System.out.println("IOException is caught");
-        }
-        catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return user;
+    /**
+     *
+     * @param username username of the query
+     * @return a User object
+     */
+    @Override
+    public User getUser(String username) {
+        return jdbcTemplate.queryForObject(DATA_QUERY, new UserDaoMapper());
     }
 
     /**
@@ -61,31 +36,20 @@ public class ConnectionDataAccess implements IConnectionDataAccess {
     public void saveUser(User user){
         try{
             ObjectMapper mapper = new ObjectMapper();
-            String connectionRequests = mapper.writeValueAsString(user.getConnectionRequests());
-            String pendingConnections = mapper.writeValueAsString(user.getPendingConnections());
-            String connections = mapper.writeValueAsString(user.getConnections());
-            jdbcTemplate.update(INSERT_QUERY, connectionRequests, pendingConnections, connections, user.getName());
+            String userString = mapper.writeValueAsString(user);
+            jdbcTemplate.update(INSERT_QUERY, user.getUsername(), userString);
         } catch(JsonProcessingException e){
-
+            System.out.println("Json process error!");
         }
     }
 
+    /**
+     *
+     * @param data new values to update, must be a stringified user object
+     * @param username username
+     */
     @Override
-    public void updateUser(ConnectionType connectionType, String values, String id) {
-        jdbcTemplate.update(UPDATE_QUERY, connectionType.toString(), values, id);
-    }
-}
-
-enum ConnectionType{
-    CONNECTION_REQUEST, PENDING_CONNECTION, CONNECTIONS;
-
-    @Override
-    public String toString() {
-        switch(this) {
-            case CONNECTION_REQUEST: return "connectionRequests";
-            case PENDING_CONNECTION: return "pendingConnections";
-            case CONNECTIONS: return "connections";
-            default: return null;
-        }
+    public void updateUser(String data, String username) {
+        jdbcTemplate.update(UPDATE_QUERY, data, username);
     }
 }
---
+++ src/main/java/user/connect/UserDaoMapper.java
@@ -0,0 +1,21 @@
+package user.connect;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entity.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDaoMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        String userData = rs.getString("data");
+        Gson gson = new Gson();
+        JsonObject object = (JsonObject) JsonParser.parseString(userData);
+        return gson.fromJson(object, User.class);
+    }
+}
