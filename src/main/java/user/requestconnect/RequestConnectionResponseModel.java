49cab653e1c85324b1980458e99625ea0d343d82
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-17 23:08:11 -0500

    make getters for payload conversion

--- src/main/java/user/requestconnect/RequestConnectionResponseModel.java
+++ src/main/java/user/requestconnect/RequestConnectionResponseModel.java
@@ -3,16 +3,17 @@ import entity.User;
 import java.util.*;
 
 public class RequestConnectionResponseModel {
-    private final ServerStatus connectionStatus;
+    private ServerStatus connectionStatus;
     private String message;
     private ArrayList<String> connectionRequests;
     private ArrayList<String> pendingConnections;
     private ArrayList<String> connections;
     private String userId;
     private String targetId;
+
     public RequestConnectionResponseModel(ServerStatus connectionStatus, String message, ArrayList<String> connectionRequests,
                                           ArrayList<String> pendingConnections, ArrayList<String> connections, String userId,
-                                           String targetId){
+                                          String targetId) {
         this.message = message;
         this.userId = userId;
         this.targetId = targetId;
@@ -21,9 +22,43 @@ public class RequestConnectionResponseModel {
         this.pendingConnections = pendingConnections;
         this.connections = connections;
     }
-    public RequestConnectionResponseModel(ServerStatus connectionStatus, String message, String userId){
+
+    public RequestConnectionResponseModel(ServerStatus connectionStatus, String message, String userId) {
         this.userId = userId;
         this.connectionStatus = connectionStatus;
         this.message = message;
+    }
+
+    public RequestConnectionResponseModel(String message, String userId) {
+        this.userId = userId;
+        this.message = message;
+    }
+
+    public ServerStatus getConnectionStatus() {
+        return connectionStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ArrayList<String> getConnectionRequests() {
+        return connectionRequests;
+    }
+
+    public ArrayList<String> getPendingConnections() {
+        return pendingConnections;
+    }
+
+    public ArrayList<String> getConnections() {
+        return connections;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getTargetId() {
+        return targetId;
     }
 }
