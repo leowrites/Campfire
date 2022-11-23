49b023fcc294a5a52b15b7fe8d95c11c0b3801cc
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-23 13:55:38 -0500

    Move connect panel to its own component

---
+++ frontend/src/Home/ConnectPanel.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import useGlobalContext from '../GlobalContext';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import useAuthContext from '../AuthContext';
+
+export default function ConnectPannel() {
+  const globalContext = useGlobalContext();
+  const [connectionInput, setConnectionInput] = useState();
+  const { principal } = useAuthContext();
+
+  return (
+    <Box>
+      <Typography variant='h5' textAlign={'start'}>
+        Search for a connection
+      </Typography>
+      <Box sx={{ display: 'flex', mt: 1 }}>
+        <TextField
+          label='username'
+          variant='filled'
+          size='small'
+          fullWidth
+          onChange={(e) => setConnectionInput(e.target.value)}
+        />
+        <Button
+          sx={{ mt: 1, ml: 1 }}
+          variant='contained'
+          onClick={() => globalContext.sendConnectionRequest(principal?.username, connectionInput)}>
+          Connect
+        </Button>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant='h5' textAlign={'start'}>
+          Your Connections
+        </Typography>
+        {principal?.user?.connections?.map((connection) => (
+          <Typography key={connection} textAlign={'start'} variant='h6'>
+            {connection}
+          </Typography>
+        ))}
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant='h5' textAlign={'start'}>
+          Your Outgoing Requests
+        </Typography>
+        {principal?.user?.outgoingConnectionRequests?.map((connection) => (
+          <Typography key={connection} textAlign={'start'} variant='h6'>
+            {connection}
+          </Typography>
+        ))}
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant='h5' textAlign={'start'}>
+          Your Incoming Requests
+        </Typography>
+        {principal?.user?.incomingConnectionRequests?.map((connection) => (
+          <Typography key={connection} textAlign={'start'} variant='h6'>
+            {connection}
+          </Typography>
+        ))}
+      </Box>
+    </Box>
+  );
+}
--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -2,23 +2,15 @@ import Container from 'react-bootstrap/Container';
 import { useEffect, useState } from 'react';
 import InternshipPaper from './InternshipPaper';
 import Grid from '@mui/material/Grid';
-import UserCard from '../Component/UserCard';
-import useGlobalContext from '../GlobalContext';
+import Box from '@mui/material/Box';
+import ConnectPanel from './ConnectPanel';
 
 function HomePage() {
   const [internships, setInternships] = useState([]);
-  const [users, setUsers] = useState([]);
-  const globalContext = useGlobalContext();
-
-  useEffect(() => {
-    fetch('/users')
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
 
   useEffect(() => {
     const internships = [];
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 5; i++) {
       internships.push({
         name: 'Apple',
         info: 'Pls give me an internship 🙏 ',
@@ -30,28 +22,18 @@ function HomePage() {
   return (
     <>
       <Container>
-        <Grid container>
-          <Grid item xs={6}>
-            {internships.map((v, i) => (
-              <InternshipPaper key={i} title={v.name} info={v.info} />
-            ))}
+        <Box sx={{ pt: 3 }}>
+          <Grid container spacing={4}>
+            <Grid item xs={8}>
+              {internships.map((v, i) => (
+                <InternshipPaper key={i} title={v.name} info={v.info} />
+              ))}
+            </Grid>
+            <Grid item xs={4}>
+              <ConnectPanel />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            {users.map((v, i) =>  (
-                <UserCard
-                  key={i}
-                  username={v.username}
-                  name={v.name}
-                  connections={v.connections}
-                  incomingConnectionRequests={v.incomingConnectionRequests}
-                  outgoingConnectionRequests={v.outgoingConnectionRequests}
-                  sendAcceptConnectionRequest={globalContext.sendAcceptConnectionRequest}
-                  sendConnectionRequest={globalContext.sendConnectionRequest}
-                />
-              )
-            )}
-          </Grid>
-        </Grid>
+        </Box>
       </Container>
     </>
   );
