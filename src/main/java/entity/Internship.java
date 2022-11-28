a1a6d094c4e4d9cf100e84b1988f8daa692c875d
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-28 00:07:22 -0500

    Add internship db access

---
+++ src/main/java/entity/Internship.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class Internship {
+    private ArrayList<String> reviews;
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
+    }
+}
