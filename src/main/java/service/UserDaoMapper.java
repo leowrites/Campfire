bd97bdf3ab1caaec62a94c3c4fffd2260e2cf9a3
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-27 19:55:13 -0500

    Move UserDaoMapper to service

--- src/main/java/service/UserDaoMapper.java
+++ src/main/java/service/UserDaoMapper.java
@@ -1,4 +1,4 @@
-package user.acceptconnect;
+package service;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
--- src/main/java/service/UserDataAccess.java
+++ src/main/java/service/UserDataAccess.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import user.acceptconnect.UserDaoMapper;
 import user.requestconnect.exceptions.UserNotFoundException;
 
 @Repository
