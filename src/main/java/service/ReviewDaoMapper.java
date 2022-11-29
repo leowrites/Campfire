6a14404978e5fb3c0cc2441b57b6cca155442b97
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-28 20:34:27 -0500

    create review dao mapper

---
+++ src/main/java/service/ReviewDaoMapper.java
@@ -0,0 +1,31 @@
+package service;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entity.Review;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReviewDaoMapper implements RowMapper<Review> {
+    /**
+     * Implementations must implement this method to map each row of data
+     * in the ResultSet. This method should not call {@code next()} on
+     * the ResultSet; it is only supposed to map values of the current row.
+     *
+     * @param rs     the ResultSet to map (pre-initialized for the current row)
+     * @param rowNum the number of the current row
+     * @return the result object for the current row (may be {@code null})
+     * @throws SQLException if an SQLException is encountered getting
+     *                      column values (that is, there's no need to catch SQLException)
+     */
+    @Override
+    public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
+        String userData = rs.getString("data");
+        Gson gson = new Gson();
+        JsonObject object = (JsonObject) JsonParser.parseString(userData);
+        return gson.fromJson(object, Review.class);
+    }
+}
