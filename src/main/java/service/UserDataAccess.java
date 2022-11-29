9f062fa8e33efa34a00dd6bc1302875c7dd04a09
leowrites <75044178+leowrites@users.noreply.github.com>
2022-11-28 20:33:44 -0500

    remove redundant code

--- src/main/java/service/UserDataAccess.java
+++ src/main/java/service/UserDataAccess.java
@@ -76,24 +76,6 @@ public class UserDataAccess implements IUserDataAccess {
     }
 
     public void reset() {
-//        ArrayList<String> user1incoming = new ArrayList<String>();
-//        ArrayList<String> user1outgoing = new ArrayList<String>();
-//        ArrayList<String> user1Connections = new ArrayList<String>();
-//        ArrayList<String> user2incoming = new ArrayList<String>();
-//        ArrayList<String> user2outgoing = new ArrayList<String>();
-//        ArrayList<String> user2Connections = new ArrayList<String>();
-//        User user1 = new User("01", user1incoming, user1Connections, user1outgoing,
-//                "leoliu", "leo@gmail.com", "pass", "Leo");
-//        User user2 = new User("02", user2incoming, user2Connections, user2outgoing,
-//                "alex", "alex@gmail.com", "pass", "Alex");
-//        User user3 = new User("01", user1incoming, user1Connections, user1outgoing,
-//                "stevejobs", "steve@gmail.com", "pass", "steve");
-//        User user4 = new User("02", user2incoming, user2Connections, user2outgoing,
-//                "timcook", "tim@gmail.com", "pass", "tim");
-//        saveUser(user1);
-//        saveUser(user2);
-//        saveUser(user3);
-//        saveUser(user4);
         ArrayList<User> users = getUsers();
         for (User user : users){
             user.setConnections(new ArrayList<>());
