ce32420e509da400715d1dfa320d8f2e58455ec1
PrimeAce123 <timothydu28apple@gmail.com>
2022-11-29 23:24:44 -0500

    Resolved merge conflicts (camel case)

--- src/main/java/user/deletereview/OwnerVerifierReview.java
+++ src/main/java/user/deletereview/OwnerVerifierReview.java
@@ -16,7 +16,7 @@ public class OwnerVerifierReview {
 
     public void verify() throws NotOwnReviewException {
         // Raises error if the two ids are not equal to each other (Comment does not belong to user)
-        String userIdComment = this.review.getUserID();
+        String userIdComment = this.review.getUserId();
         if (!this.userId.equals(userIdComment)) {
             throw new NotOwnReviewException("Review does not belong to user");
         }
