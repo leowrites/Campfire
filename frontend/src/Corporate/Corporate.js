6857b61b60465a126e7f18e704cdfe54aa051945
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-02 15:35:32 -0500

    move components

--- frontend/src/Corporate/Corporate.js
+++
@@ -1,6 +0,0 @@
-export default function Corporate() {
-    return (
-        <>
-        </>
-    )
-}
--- frontend/src/Internship/Internship.js
+++
@@ -1,6 +0,0 @@
-export default function Internship() {
-    return (
-        <>
-        </>
-    )
-}
