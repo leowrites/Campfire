5402ff0ceb8156b9722439ee28e9d352bc591967
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-01 20:05:32 -0500

    update constructor and fixed tests

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -7,27 +7,23 @@ public class User implements Serializable {
     private ArrayList<String> incomingConnectionRequests;
     private ArrayList<String> outgoingConnectionRequests;
     private ArrayList<String> connections;
-    private String id;
     private String name;
     private String username;
     private String email;
     private String password;
     private int accessLevel;
     public User() {}
-    public User(String id,
-                ArrayList<String> incomingConnectionRequests,
-                ArrayList<String> outgoingConnectionRequests,
-                ArrayList<String> connections,
+
+    public User(
                 String username,
                 String email,
                 String password,
                 String name) {
-        this.incomingConnectionRequests = incomingConnectionRequests;
-        this.outgoingConnectionRequests = outgoingConnectionRequests;
-        this.connections = connections;
+        this.incomingConnectionRequests = new ArrayList<>();
+        this.outgoingConnectionRequests = new ArrayList<>();
+        this.connections = new ArrayList<>();
         this.username = username;
         this.email = email;
-        this.id = id;
         this.password = password;
         this.name = name;
         this.accessLevel = 0;
@@ -56,9 +52,6 @@ public class User implements Serializable {
     public void setConnections(ArrayList<String> connections) {
         this.connections = connections;
     }
-    public String getId(){
-        return id;
-    }
     public String getName() {
         return name;
     }
@@ -71,13 +64,6 @@ public class User implements Serializable {
     public String getEmail() {
         return email;
     }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAccessLevel(){return this.accessLevel;}
 
     public void setAccessLevel(int accessLevel){
