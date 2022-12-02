b29301e52b5eb728623921ff53ffa354ebb0c13c
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-02 00:10:46 -0500

    add axios requests

--- frontend/src/Component/ReviewCard.js
+++ frontend/src/Component/ReviewCard.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import CommentBox from './CommentBox';
+import { useParams } from 'react-router-dom';
 
 export default function ReviewCard({
+  reviewId,
   userId,
   datePosted,
   numLikes,
@@ -11,6 +17,21 @@ export default function ReviewCard({
   comments,
   rating,
 }) {
+  const { corporateId, internshipId } = useParams();
+  const [showComment, setShowComment] = useState(false);
+  const handleDelete = () => {
+    axios.delete(`/corporates/${corporateId}/internships/${internshipId}`, {
+      internshipId: internshipId,
+      reviewId: reviewId,
+      userId: userId,
+    });
+  };
+  const handleShowCommentBox = () => {
+    setShowComment(!showComment);
+  };
+  const fetchComments = () => {
+    
+  }
   return (
     <Paper elevation={2} sx={{ mb: 3, p: 3 }}>
       <Box textAlign='start'>
@@ -21,7 +42,20 @@ export default function ReviewCard({
         <Typography>Review: {content}</Typography>
         <Typography>{comments}</Typography>
         <Typography>Rating: {rating}/10</Typography>
+        <Button variant='contained' onClick={handleShowCommentBox} color='success'>
+          Leave a comment
+        </Button>
+        <Button variant='contained' onClick={handleDelete} color='error'>
+          Delete this review
+        </Button>
       </Box>
+      {showComment ? (
+        <Box sx={{ mt: 2 }}>
+          <CommentBox handleShowCommentBox={handleShowCommentBox} />
+        </Box>
+      ) : (
+        undefined
+      )}
     </Paper>
   );
 }
