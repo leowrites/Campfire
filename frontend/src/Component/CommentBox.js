edb4db7e3998651e6747c54adef4e82f40ff57f8
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-02 02:03:29 -0500

    implement comment frontend

--- frontend/src/Component/CommentBox.js
+++ frontend/src/Component/CommentBox.js
@@ -1,32 +1,26 @@
-import Box from "@mui/material/Box"
-import TextField from "@mui/material/TextField"
-import axios from "axios"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
 import { useState } from 'react';
-import { useParams } from "react-router-dom";
-import Button from "@mui/material/Button";
-import useAuthContext from "../AuthContext";
+import { useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import useAuthContext from '../AuthContext';
 
-export default function CommentBox({ handleShowCommentBox, userId, parentType, parentId }) {
-    const [comment, setComment] = useState('')
-    const { corporateId, internshipId } = useParams();
-    const authContext = useAuthContext();
-    const principal = authContext.principal;
-    const postComment = () => {
-        axios.post(`/corporates/${corporateId}/internships/${internshipId}`, {
-            userId: principal.username,
-            parentType: parentType,
-            parentId: parentId,
-            content: comment,
-        })
-    }
-    const handleValChange = (e) => {
-        setComment(e.target.value)
-    }
-    return (
-        <Box>
-            <TextField fullWidth onChange={handleValChange}/>
-            <Button onClick={postComment}>post</Button>
-            <Button onClick={handleShowCommentBox}>Close</Button>
-        </Box>
-    )
+export default function CommentBox({ handleShowCommentBox, postComment, parentType, parentId }) {
+  const [comment, setComment] = useState('');
+  const handleValChange = (e) => {
+    setComment(e.target.value);
+  };
+  return (
+    <Box>
+      <TextField fullWidth onChange={handleValChange} />
+      <Button
+        onClick={() => {
+          postComment(parentType, parentId, comment);
+        }}>
+        post
+      </Button>
+      <Button onClick={handleShowCommentBox}>Close</Button>
+    </Box>
+  );
 }
--- frontend/src/Component/CommentCard.js
+++ frontend/src/Component/CommentCard.js
@@ -1,35 +1,35 @@
 import { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import axios from 'axios';
-import CommentBox from './CommentBox';
 import { useParams } from 'react-router-dom';
-import CommentCard from './CommentCard';
-import useAuthContext from "../AuthContext";
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+import CommentBox from './CommentBox';
 
-export default function ReviewCard({
-  reviewId,
+export default function CommentCard({
+  commentId,
   userId,
-  datePosted,
-  numLikes,
-  numDislikes,
+  parentType,
+  reviewId,
+  parentId,
   content,
+  datePosted,
   comments,
-  rating,
 }) {
   const { corporateId, internshipId } = useParams();
   const [showComment, setShowComment] = useState(false);
   const [moreComments, setMoreComments] = useState([]);
-  const authContext = useAuthContext();
-  const principal = authContext.principal;
   const handleDelete = () => {
-    axios.delete(`/corporates/${corporateId}/internships/${internshipId}`, {
-      internshipId: internshipId,
-      reviewId: reviewId,
-      userId: userId,
-    });
+    axios.delete(
+      `/corporates/${corporateId}/internships/${internshipId}/reviews/${reviewId}/comments`,
+      {
+        commentId: commentId,
+        parentType: parentType,
+        parentId: parentId,
+        userId: userId,
+      }
+    );
   };
   const handleShowCommentBox = () => {
     setShowComment(!showComment);
@@ -50,39 +50,15 @@ export default function ReviewCard({
     );
   };
 
-  const postComment = (parentType, parentId, comment) => {
-    axios.post(`/comments`, {
-        userId: principal.username,
-        parentType: parentType,
-        parentId: parentId,
-        content: comment,
-    })
-    .then(res => {
-      if (res.data.status === "SUCCESS") {
-        setShowComment(false);
-        setMoreComments([...moreComments, {
-          commentId: res.data.commentId,
-          content: comment,
-          comments: [],
-          datePosted: res.data.datePosted
-        }]);
-      }
-    })
-}
-
   return (
-    <Paper elevation={2} sx={{ mb: 3, p: 3 }}>
-      <Box textAlign='start'>
-        <Box sx={{ display: 'flex' }}>
-          <Typography variant='h5'>{userId}: </Typography>
-          <Typography sx={{ display: 'flex', ml: 'auto' }}>👍 {numLikes}</Typography>
-          <Typography>👎 {numDislikes}</Typography>
-        </Box>
+    <Paper elevation={2} sx={{ my: 3, p: 3 }}>
+      <Box textAlign={'start'}>
+        <Typography variant='h5'>{userId}: </Typography>
         <Typography>{content}</Typography>
-        <Typography>Rating: {rating}/10</Typography>
         <Box sx={{ display: 'flex' }}>
-          <Typography sx={{ display: 'flex', ml: 'auto' }}>Date Posted: {datePosted}</Typography>
+          <Typography sx={{ ml: 'auto' }}>Date Posted: {datePosted}</Typography>
         </Box>
+
         <Box sx={{ display: 'flex', my: 1 }}>
           <Button
             sx={{ ml: 'auto', mr: 1 }}
@@ -93,34 +69,29 @@ export default function ReviewCard({
             comment
           </Button>
           <Button variant='contained' onClick={handleDelete} color='error' size='small'>
-            delete
+            Delete
           </Button>
         </Box>
       </Box>
       {showComment ? (
         <Box sx={{ mt: 2 }}>
-          <CommentBox
-            handleShowCommentBox={handleShowCommentBox}
-            parentType={'Review'}
-            parentId={reviewId}
-            postComment={postComment}
-          />
+          <CommentBox handleShowCommentBox={handleShowCommentBox} />
         </Box>
       ) : undefined}
 
       {moreComments.length > 0 &&
-        moreComments.map((comment, i) => {
+        moreComments.map((comment) => {
           return (
             <CommentCard
-              key={i}
+              key={comment.commentId}
               reviewId={reviewId}
               parentType={'Review'}
               commentId={comment.commentId}
-              parentId={reviewId}
+              parentId={comment.commentI}
               userId={userId}
               content={comment.content}
               datePosted={comment.datePosted}
-              comments={comment.comments}
+              comments={comments}
             />
           );
         })}
