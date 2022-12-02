6eaa3d80cb4b2fd52281bcc772d5849489645756
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-02 02:01:50 -0500

    add info to response

--- src/main/java/user/comment/CommentResponseModel.java
+++ src/main/java/user/comment/CommentResponseModel.java
@@ -4,10 +4,14 @@ import service.ServerStatus;
 public class CommentResponseModel {
     private final ServerStatus status;
     private final String message;
+    private final int commentId;
+    private final String datePosted;
 
-    public CommentResponseModel(ServerStatus status, String message) {
+    public CommentResponseModel(ServerStatus status, String message, int commentId, String datePosted) {
         this.status = status;
         this.message = message;
+        this.commentId = commentId;
+        this.datePosted = datePosted;
     }
 
     public ServerStatus getStatus() {
@@ -17,4 +21,13 @@ public class CommentResponseModel {
     public String getMessage() {
         return this.message;
     }
+
+    public int getCommentId() {
+        return this.commentId;
+    }
+    
+    public String getDatePosted() {
+        return this.datePosted;
+    }
+
 }
