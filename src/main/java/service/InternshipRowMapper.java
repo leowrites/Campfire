37a98d28ca1257c8e812848cdf024f94ec3b7e96
MinGi-K <mingikwon1106@gmail.com>
2022-12-02 22:49:34 -0500

    Changes by siqi

--- src/main/java/service/InternshipRowMapper.java
+++ src/main/java/service/InternshipRowMapper.java
@@ -1,4 +1,8 @@
 package service;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entity.Internship;
 
 import entity.User;
