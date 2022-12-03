04712e4a0ba1725568b0a6ee118320d6f27a10b3
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-03 00:02:16 -0500

    add review box for internship

--- frontend/src/Internship/InternshipPage.js
+++ frontend/src/Internship/InternshipPage.js
@@ -5,22 +5,54 @@ import Box from '@mui/material/Box';
 import { useParams } from 'react-router-dom';
 import ReviewCard from './ReviewCard';
 import Button from '@mui/material/Button';
+import CommentCard from '../Internship/CommentBox';
+import useAuthContext from '../AuthContext';
+import Rating from '@mui/material/Rating';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function InternshipPage() {
   const [reviews, setReviews] = useState([]);
   const { corporateId, internshipId } = useParams();
+  const [showCommentBox, setShowCommentBox] = useState(false);
+  const auth = useAuthContext();
+  const handleShowCommentBox = () => {
+    setShowCommentBox(!showCommentBox);
+  };
   useEffect(() => {
     axios.get(`/corporates/${corporateId}/internships/${internshipId}/reviews`).then((data) => {
       console.log(data);
       setReviews(data.data);
     });
   }, []);
-  // allow user to add a review through a button
+
+  const postComment = (comment, rating) => {
+    axios.post(`/corporates/${corporateId}/internships/${internshipId}/reviews`, {
+      reviewContent: comment,
+      username: auth.username,
+      rating: rating,
+    });
+  };
+
   return (
     <Box sx={{ my: 2 }} textAlign='start'>
       <Typography variant='h4'>Internship Id: {internshipId}</Typography>
-      <Button>Add a review</Button>
-
+      <Button onClick={handleShowCommentBox}>Add a review</Button>
+      {showCommentBox ? (
+        <Box>
+          <Rating
+            defaultValue={0}
+            icon={<FavoriteIcon fontSize='inherit' />}
+            emptyIcon={<FavoriteBorderIcon fontSize={'inherit'} />}
+          />
+          <CommentCard
+            handleShowCommentBox={handleShowCommentBox}
+            postComment={postComment}
+            parentType={'Review'}
+            parentId={internshipId}
+          />
+        </Box>
+      ) : null}
       {reviews?.map((review) => (
         <ReviewCard id={review.id} />
       ))}
