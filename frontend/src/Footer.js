0ca329ab781f9d8a4afe792da2563824a741ca6e
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-03 14:55:27 -0500

    add footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,14 +1,12 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import useAuthContext from "./AuthContext";
+import useAuthContext from './AuthContext';
 
 export default function Footer() {
+  const { principal } = useAuthContext();
 
-
-    const { principal } = useAuthContext();
-
-    // a footer that spans the entire width of the page
+  // a footer that spans the entire width of the page
   // has a prompt to allow company representatives to add their company
   // create a beautiful footer with some nice colors
   return (
@@ -25,11 +23,13 @@ export default function Footer() {
       }}>
       <Typography sx={{ display: 'flex' }} variant='h7'>
         Company representitive?
-
-          {principal ? <Link to={`/corporates/create`} style={{ color: 'gray', textDecoration: 'none' }}>
-              Add your company
-          </Link> : <></> }
-
+        {principal ? (
+          <Link to={`/corporates/create`} style={{ color: 'gray', textDecoration: 'none' }}>
+            Add your company
+          </Link>
+        ) : (
+          <></>
+        )}
       </Typography>
       <Typography sx={{ display: 'flex' }} variant='h7'>
         My Intern Days 2022
