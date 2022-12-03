94ce31ad50cf578080d8b36d053e04aa3ed8b25a
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-03 16:13:33 -0500

    update footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -10,30 +10,32 @@ export default function Footer() {
   // has a prompt to allow company representatives to add their company
   // create a beautiful footer with some nice colors
   return (
-    <Box
-      sx={{
-        mt: 2,
-        p: 2,
-        position: 'fixed',
-        left: 0,
-        bottom: 0,
-        right: 0,
-        backgroundColor: '#eff1f3',
-        minHeight: '5rem',
-      }}>
-      <Typography sx={{ display: 'flex' }} variant='h7'>
-        Company representitive?
-        {principal ? (
-          <Link to={`/corporates/create`} style={{ color: 'gray', textDecoration: 'none' }}>
-            Add your company
-          </Link>
-        ) : (
-          <></>
-        )}
-      </Typography>
-      <Typography sx={{ display: 'flex' }} variant='h7'>
-        My Intern Days 2022
-      </Typography>
+    <Box sx={{ mt: '10rem' }}>
+      <Box
+        sx={{
+          mt: 2,
+          p: 2,
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          right: 0,
+          backgroundColor: '#eff1f3',
+          minHeight: '5rem',
+        }}>
+        <Typography sx={{ display: 'flex' }} variant='h7'>
+          Company representitive?
+          {principal ? (
+            <Link to={`/corporates/create`} style={{ color: 'gray', textDecoration: 'none' }}>
+              Add your company
+            </Link>
+          ) : (
+            <></>
+          )}
+        </Typography>
+        <Typography sx={{ display: 'flex' }} variant='h7'>
+          My Intern Days 2022
+        </Typography>
+      </Box>
     </Box>
   );
 }
