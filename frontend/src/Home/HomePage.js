d58966d0fb5341e8ec30261476734f2f71dd73ec
MinGi-K <mingikwon1106@gmail.com>
2022-12-04 12:45:14 -0500

    Added more images to home page detailing what users can do on the platform. Removed footer margin top.

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -4,7 +4,9 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from "@mui/material/TextField";
 import Image from './bannerImage.png';
-
+import {ReactComponent as BonfireSVG} from './bonfire.svg'
+import {ReactComponent as CommentsSVG} from './comments.svg'
+import {ReactComponent as ConnectSVG} from './connect.svg'
 
 
 function HomePage() {
@@ -54,8 +56,40 @@ function HomePage() {
           }} />
 
       </Grid>
-        <Grid container sx={{backgroundColor:'blue', height: '30vh', width: '100vw'}}>
-
+        <Grid container
+              sx={{backgroundColor:'#fcd4bd',
+                  height: '60vh',
+                  width: '100vw'}}>
+            <Grid item xs={4} alignItems="center" justifyContent="center" sx={{padding: '2%'}}>
+                <Typography sx={{
+                    fontFamily: 'arial',
+                    fontWeight: 'bold',
+                    color: '#8c1c0d',
+                }}>
+                    You've reached an end of an important journey. Stop by and recount your valuable experience.
+                </Typography>
+                <BonfireSVG style={{width: '18vw', height:'18vw'}}/>
+            </Grid>
+            <Grid item xs={4} alignItems="center" justifyContent="center" sx={{padding: '2%'}}>
+                <Typography sx={{
+                    fontFamily: 'arial',
+                    fontWeight: 'bold',
+                    color: '#8c1c0d',
+                }}>
+                    Curious about someone's experience? Interact on our platform by leaving a comment.
+                </Typography>
+                <CommentsSVG style={{width: '18vw', height:'18vw'}}/>
+            </Grid>
+            <Grid item xs={4} alignItems="center" justifyContent="center" sx={{padding: '2%'}}>
+                <Typography sx={{
+                    fontFamily: 'arial',
+                    fontWeight: 'bold',
+                    color: '#8c1c0d',
+                }}>
+                    Connect to other users to ask deeper, or other burning questions privately.
+                </Typography>
+                <ConnectSVG style={{width: '18vw', height:'18vw'}}/>
+            </Grid>
         </Grid>
     </>
 
