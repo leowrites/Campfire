290bf9564ab00aec6b4062d0f8749d053325b6cd
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-21 16:24:36 -0500

    add cool down to post buttons

--- frontend/src/Internship/CommentBox.js
+++ frontend/src/Internship/CommentBox.js
@@ -4,9 +4,19 @@ import Button from '@mui/material/Button';
 import CustomTextField from '../Component/CustomTextfield';
 import useTheme from '@mui/material/styles/useTheme';
 
-export default function CommentBox({ handleShowCommentBox, postComment, parentType, parentId }) {
+export default function CommentBox({ handleShowCommentBox, postComment, parentType, parentId, rating }) {
   const [comment, setComment] = useState('');
+  const [clicked, setClicked] = useState(false);
   const theme = useTheme();
+  const handleClickPost = () => {
+    setClicked(true)
+    parentType === 'Internship'
+    ? postComment(comment, rating)
+    : postComment(parentType, parentId, comment);
+    setTimeout(() => {
+      setClicked(false)
+    }, 3000)
+  }
   const handleValChange = (e) => {
     setComment(e.target.value);
   };
@@ -24,11 +34,8 @@ export default function CommentBox({ handleShowCommentBox, postComment, parentTy
         <Button
           size='small'
           sx={{ background: theme.palette.primary.main, color: 'white', mr: 1, ml: 'auto' }}
-          onClick={() => {
-            parentType === 'Internship'
-              ? postComment(comment, 0)
-              : postComment(parentType, parentId, comment);
-          }}>
+          disabled={clicked}
+          onClick={handleClickPost}>
           post
         </Button>
         <Button
