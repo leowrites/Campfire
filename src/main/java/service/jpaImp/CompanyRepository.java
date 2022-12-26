00b8a5ea850199b81a5495256224c83358d9bbf5
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-26 17:51:29 -0500

    Hibernate implementation for ICorporateDAO

---
+++ src/main/java/service/jpaImp/CompanyRepository.java
@@ -0,0 +1,10 @@
+package service.jpaImp;
+
+import entity.Corporate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Corporate, UUID> {
+}
