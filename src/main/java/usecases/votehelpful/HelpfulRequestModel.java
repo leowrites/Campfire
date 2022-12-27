5a9e9dbeefe7f7bb5a85bf0ee60a4576b7c67600
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-27 01:45:47 -0500

    update id from int to UUID

--- src/main/java/usecases/votehelpful/HelpfulRequestModel.java
+++ src/main/java/usecases/votehelpful/HelpfulRequestModel.java
@@ -1,15 +1,17 @@
 package usecases.votehelpful;
 
+import java.util.UUID;
+
 /** A request model for the votehelpful use case that frames the input data into an object. Holds
  * a boolean representation of if the review is helpful or not in isHelpful, and an int
  * representation of the review's id in reviewId.
  */
 public class HelpfulRequestModel {
     private final String isHelpful;
-    private final int reviewId;
+    private final UUID reviewId;
     private final String userId;
 
-    public HelpfulRequestModel(String isHelpful, int reviewId, String userId) {
+    public HelpfulRequestModel(String isHelpful, UUID reviewId, String userId) {
         this.isHelpful = isHelpful;
         this.reviewId = reviewId;
         this.userId = userId;
@@ -19,7 +21,7 @@ public class HelpfulRequestModel {
         return this.isHelpful;
     }
 
-    public int getReviewId() {
+    public UUID getReviewId() {
         return this.reviewId;
     }
 
