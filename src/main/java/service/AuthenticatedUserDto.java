bb32e751dac85a214b408736d933a7d9512ec5e6
leowrites <75044178+leowrites@users.noreply.github.com>
2022-12-31 19:49:25 -0500

    add dto for authenticated user

---
+++ src/main/java/service/AuthenticatedUserDto.java
@@ -0,0 +1,61 @@
+package service;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import entity.User;
+
+import java.util.List;
+
+public class AuthenticatedUserDto {
+    private String username;
+
+    private String email;
+    private String name;
+    @JsonIncludeProperties("username")
+    private List<User> connectedUsers;
+
+    @JsonIncludeProperties("username")
+    private List<User> incomingConnectionRequests;
+    @JsonIncludeProperties("username")
+    private List<User> outgoingConnectionRequests;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getConnectedUsers() {
+        return connectedUsers;
+    }
+
+    public void setConnectedUsers(List<User> connectedUsers) {
+        this.connectedUsers = connectedUsers;
+    }
+
+    public List<User> getIncomingConnectionRequests() {
+        return incomingConnectionRequests;
+    }
+
+    public void setIncomingConnectionRequests(List<User> incomingConnectionRequests) {
+        this.incomingConnectionRequests = incomingConnectionRequests;
+    }
+
+    public List<User> getOutgoingConnectionRequests() {
+        return outgoingConnectionRequests;
+    }
+
+    public void setOutgoingConnectionRequests(List<User> outgoingConnectionRequests) {
+        this.outgoingConnectionRequests = outgoingConnectionRequests;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
